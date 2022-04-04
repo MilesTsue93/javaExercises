de4fd96e68b76d45566c37430ac0cc916ff44cf8
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-04-04 14:37:17 -0400

    added recursion problem.

--- Squares.java
+++ Squares.java
@@ -3,12 +3,31 @@ public class Squares {
     
     public static void main(String[] args) {
         
-        int square = 0;
+        // squaring exercise
+        //square10();
 
+        // fibonacci exercise
+        fib(4);
+    }
+
+    public static void square10() {
+        
+        int square = 0;
         // the squares of ten numbers without using * operator!
         for (int i = 1; i <= 20; i += 2) {
             square += i; 
             System.out.print(square + " ");
         }
     }
+
+    // a standard recursion problem. I used a parameter and returned an integer.
+    public static int fib(int n) {
+        
+        if (n <= 2) {
+             return 1;
+        }
+        return fib(n-1) + fib(n-2);      
+    }
 }
+
+
