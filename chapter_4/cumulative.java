b635fc39b645241bf35adeba88cc76d89bd52145
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-05-04 20:56:45 -0400

    changed nextBoolean to next()

--- chapter_4/cumulative.java
+++ chapter_4/cumulative.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 
 
 public class cumulative {
-    public static final String USAGE = "Usage [true/false]";
+    public static final String USAGE = "Usage [y/n]";
+    
 
     public static void main(String[] args) {
         // define Scanner object
         Scanner console = new Scanner(System.in);
 
         // calculate the product
-        int result = productMaker(console);
-        System.out.println("Here is your product: " + result);
+        //int result = productMaker(console);
+        //System.out.println("Here is your product: " + result);
+
+        // declare whether even or odd
+        evenOrOdd(console);
     }
 
     public static int productMaker(Scanner console) {
@@ -22,28 +26,72 @@ public class cumulative {
 
 
         System.out.print("do you want to multiply this number [true/false]? ");
-        boolean choice = console.nextBoolean();
+        String choice = console.next();
 
         // until user writes 'false' in console
-        while (choice == true) {
+        while (choice.equals("y")) {
 
             // user chooses a factor
             System.out.print("Give me a number to multiply your last number by: ");
             int factor = console.nextInt();
             
-            if(choice == false) {
+            if(choice.equals("n")) {
                 return product;
             }
             product *= factor;  
 
             // user chooses whether to continue multiplying number.
             System.out.print("Do you want to continue accumulating? [true/false]");
-            choice = console.nextBoolean();
+            choice = console.next();
 
-            if (choice != true && choice != false) {
+            if (!(choice.equals("y") && choice.equals("n"))) {
                 throw new IllegalArgumentException(USAGE);
             }
         }
         return product;
+    }
+
+    public static void evenOrOdd(Scanner console) {
+        
+        // declare user inputs
+        System.out.print("What's your number, gurl??? ");
+        int num = console.nextInt();
+
+        // first number
+        if (num % 2 == 0) {
+            System.out.print("even\n");
+        } else if (num % 2 != 0) {
+            System.out.print("odd\n");
+        }
+        System.out.println();
+
+        // only if true will code continue
+        System.out.print("do you want to pick another??? ");
+        String choice = console.next();
+
+        //loop through this process until terminated by user
+        while(choice.equals("y")) {
+
+            // declare user inputs
+            System.out.print("What's your number, gurl??? ");
+            num = console.nextInt();
+
+            if(num % 2 == 0) {
+                System.out.println("even");
+            } else if (num % 2 != 0) {
+                System.out.println("odd");
+            }
+
+            System.out.print("do you want to pick another??? ");
+            choice = console.next();
+
+            if (choice.equals("n")) {
+                System.out.println("Have a nice day.");
+            }
+            if (!choice.equals("y") && !choice.equals("n")) {
+                throw new IllegalArgumentException(USAGE);
+            }
+        }
+
     }
 }
