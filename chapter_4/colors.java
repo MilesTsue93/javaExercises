cdbb7f05b31921731e22527144357c3641a1182d
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-05-12 11:22:51 -0400

    added new file.

---
+++ chapter_4/colors.java
@@ -0,0 +1,44 @@
+package chapter_4;
+
+import java.util.Scanner;
+public class colors {
+
+    // constant for proper input
+    public static final String USAGE = "Usage: [B/G/R]";
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String choice = chooseColor(console);
+        String choice2 = chooseColor(console);
+
+        System.out.println("You have chosen " + choice);
+        System.out.println("You have chosen " + choice2);
+    }
+
+    // method for lengthening the char version of color
+    // into the actual color name
+    public static String chooseColor(Scanner console) {
+        System.out.print("What color do you want? ");
+
+        //will always be uppercase whether it's lower or upper case letter
+        String color = console.next().toUpperCase();
+
+        if (color == "B") {
+            color += "lue";
+            return color;
+        } else if (color == "G") {
+            color += "reen";
+            return color;
+        } else if (color == "R") {
+            color += "ed";
+            return color;
+        } else {
+            System.out.print("Unknown color: " + color);
+            return color;
+        } 
+
+    }
+
+
+    
+}
