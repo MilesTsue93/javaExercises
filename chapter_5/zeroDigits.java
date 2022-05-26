9ee9e6474ac963c874136da554e669971b662375
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-05-25 22:46:31 -0400

    added one more static method within main method.

--- chapter_5/zeroDigits.java
+++ chapter_5/zeroDigits.java
@@ -2,7 +2,6 @@ package chapter_5;
 import java.util.Scanner;
 
 public class zeroDigits {
-
     public static void main(String[] args) {
 
 
@@ -21,5 +20,36 @@ public class zeroDigits {
         } while (num != 0);
 
         System.out.println("Your number had " + count + " zeros in it.");
-    }
+
+
+        // here is a separate method altogether, unrelated to this first one. //
+
+        // prompt user for an integer
+        System.out.print("Type an integer: ");
+        int number = console.nextInt();
+
+        String newNumOdd = "";
+        String newNumEven = "";
+        //process digits one at a time checking for odd or even
+        // process odds first, then evens, then
+        //concatenate them, then finally, conver to integer again.
+        while (number != 0) {
+            if ((number % 10) % 2 != 0) {
+                newNumOdd = (number % 10) + newNumOdd;
+                number /= 10;
+            } else if ((number % 10) % 2 == 0) {
+                newNumEven = (number % 10) + newNumEven;
+                number /= 10;
+            }
+        }
+
+        String newNumber = newNumOdd + newNumEven;
+        System.out.println("Here is your new number: " + newNumber);
+
+
+
+
+    }    
 }
+
+
