c80505537d4c5deb9653aff22a1824c27f292424
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-07-05 15:44:53 -0400

    modified doWhile.java file.

--- chapter_5/SentinelLoop.java
+++ chapter_5/SentinelLoop.java
@@ -6,9 +6,9 @@
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
+        
 
-
-        sentinel(console);
+        //sentinel(console);
 
     }
 
--- chapter_5/doWhile.java
+++ chapter_5/doWhile.java
@@ -1,5 +1,26 @@
 package chapter_5;
+import java.util.Scanner;
+import java.util.Random;
 
 public class doWhile {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        // prompt for message
+        System.out.print("A message? ");
+        String message = console.nextLine();
+        String answer;
+        do {
+            System.out.println(message);
+            System.out.print("would you like to hear it again? [y/n] ");
+            answer = console.next();
+        } while (answer.equals("y"));
+
+        //prompt for numbers. Will keep prompting until a number is greater than 900
+        Random rand = new Random();
+        int number;
+        do {
+            number = rand.nextInt(1001);
+            System.out.println("Random Number: " + number);
+        } while (number < 900);
+    }
 }
