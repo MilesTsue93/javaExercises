4698b8466328949bb7d32a1a72de4b7eb0ab979f
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2022-07-18 00:44:04 -0400

    added another multidimensional array exercise.

--- chapter_7/Multi.java
+++ chapter_7/Multi.java
@@ -1,13 +1,33 @@
 package chapter_7;
-import java.util.Arrays;
+import java.util.*;
 
 public class Multi {
     public static void main (String[] args) {
         int[][] arr = new int[3][7];
-        for (int i = 1; i < arr[2].length; i++) {
-            arr[2][i] = i;
+        for (int i = 0; i < 7; i++) {
+            arr[2][i] = i+1;
         }
 
         System.out.println("array's third row looks like this: " + Arrays.toString(arr[2]));
+        int[][] arr2 = {
+            {1,2,3,4,5,6,7,8,9,0},
+            {1,2,3,4,5,6,7,8,9,0},
+            {1,2,3,4,5,6,7,8,9,0},
+            {1,2,3,4,5,6,7,8,9,0},
+            {1,2,3,4,5,6,7,8,9,0}
+        };
+        
+        multTable(arr2);
+    }
+    
+    public static void multTable(int[][] arr2) {
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                arr2[i][j] = i*j;
+            }
+
+            System.out.println(Arrays.deepToString(arr2));
+
+        }
     }
 }
